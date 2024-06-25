651fd664f1928b2730af2c36f071c2020d94ab4d
sami Um <sammiiiullah@gmail.com>
2024-06-24 19:56:39 -0700

    nav menu for small width device add

---
+++ script.js
@@ -0,0 +1,10 @@
+let icon = document.querySelector(".menu")
+
+let menus = document.querySelector(".nav-menu")
+
+
+
+icon.addEventListener("click" , function() {
+        menus.classList.toggle('visible');
+
+})
